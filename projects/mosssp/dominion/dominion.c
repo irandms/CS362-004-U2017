e80d283873f1acece94be6b924ba81bd43955e32
Spencer Michael Moss <radio@another.computer>
2017-07-09 14:26:13 -0700

    Refactored Council Room card into its own function

--- projects/mosssp/dominion/dominion.c
+++ projects/mosssp/dominion/dominion.c
@@ -725,6 +725,35 @@ int playSalvager(struct gameState *state, int choice1, int handPos)
     return 0;
 }
 
+int playCouncilRoom(struct gameState *state, int handPos)
+{
+    int currentPlayer = whoseTurn(state);
+
+    //+4 Cards
+    int i;
+    for (i = 0; i < 4; i++)
+    {
+        drawCard(currentPlayer, state);
+    }
+        
+    //+1 Buy
+    state->numBuys++;
+        
+    //Each other player draws a card
+    for (i = 0; i < state->numPlayers; i++)
+    {
+        if ( i != currentPlayer )
+        {
+            drawCard(i, state);
+        }
+    }
+    
+    //put played card in played card pile
+    discardCard(handPos, currentPlayer, state, 0);
+    
+    return 0;
+}
+
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
   int i;
@@ -749,28 +778,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
         return playAdventurer(state);
 			
     case council_room:
-      //+4 Cards
-      for (i = 0; i < 4; i++)
-	{
-	  drawCard(currentPlayer, state);
-	}
-			
-      //+1 Buy
-      state->numBuys++;
-			
-      //Each other player draws a card
-      for (i = 0; i < state->numPlayers; i++)
-	{
-	  if ( i != currentPlayer )
-	    {
-	      drawCard(i, state);
-	    }
-	}
-			
-      //put played card in played card pile
-      discardCard(handPos, currentPlayer, state, 0);
-			
-      return 0;
+        return playCouncilRoom(state, handPos);
 			
     case feast:
       //gain card with cost up to 5
